eb903165e5c58b89b9db26d840b1f24cd8aee465
Farhad-Rezwan <farhad.rezwan@gmail.com>
2020-04-02 20:48:09 +1100

    Used SNS when the portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,25 +7,33 @@ import mimetypes
 
 def lambda_handler(event, context):
     # TODO implement
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:976452920530:deployPortfolioTopic')
+
+    try:
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('portfolio.farhadrezwan.com')
+        build_bucket = s3.Bucket('portfoliobuild.farhadrezwan.com')
+
+        portfolio_zip = BytesIO()
+        build_bucket.download_fileobj('portfolioBuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print('Job Done!')
+        topic.publish(Subject = 'Portfolio Deployment', Message='Portfolio Deployed Successfully.')
+
+    except:
+        topic.publish(Subject = 'Portfolio Deployment Failed', Message='Portfolio Deployed was unsuccessfull.')
+        raise
 
 
-    s3 = boto3.resource('s3')
 
-
-    portfolio_bucket = s3.Bucket('portfolio.farhadrezwan.com')
-    build_bucket = s3.Bucket('portfoliobuild.farhadrezwan.com')
-
-    portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('portfolioBuild.zip', portfolio_zip)
-
-
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
     return {
         'statusCode': 200,
