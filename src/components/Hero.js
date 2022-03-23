920ee32dfdf03b8b02f03de3ccfd73d42aa7175c
Farhad-Rezwan <farhad.rezwan@gmail.com>
2022-03-24 00:14:19 +1100

    Add last name in name

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -27,7 +27,7 @@ const Hero = () => {
       <div className="section-center hero-center">
         <article className="hero-info">
           <div className="underline"></div>
-          <h1>I AM FARHAD</h1>
+          <h1>I AM FARHAD Rezwan</h1>
           <h4>cloud fluent full-stack developer | </h4>
           <h4>Blockchain Enthusiast</h4>
           <Link to="/contact" className="btn">
